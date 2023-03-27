4a2c844bb696d97d9d38c1e0b880c34c02ba8369
yaswanth <ya8wanth@gmail.com>
2023-03-27 16:20:30 +0530

    Spiral program completed

--- SpiralMatrix/Program.cs
+++ SpiralMatrix/Program.cs
@@ -1,46 +1,45 @@
 ﻿namespace MyProject;
 class Program
 {
-    static void display(int[,] matrix, int rows, int columns)
-    {
+    static void Main(string[] args){
+         int[,] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+        int rows = 3;
+        int cols = 4;
         int top = 0;
         int bottom = rows - 1;
         int left = 0;
-        int right = columns - 1;
+        int right = cols - 1;
         while (top <= bottom && left <= right){
 
             for (int i = left; i <= right; i++){
                 Console.Write(matrix[top, i] + " ");
             }
             top++;
-            Console.WriteLine();
+
             for (int i = top; i <= bottom; i++){
                 Console.Write(matrix[i, right] + " ");
             }
             right--;
-            Console.WriteLine();
+
             if (top <= bottom){
-            for (int i = right; i >= left; i--){
-                Console.Write(matrix[bottom, i] + " ");
-             }
-            bottom--;
+                for (int i = right; i >= left; i--)
+                {
+                    Console.Write(matrix[bottom, i] + " ");
+                }
+                bottom--;
             }
-            Console.WriteLine();
-            if (left <= right){
-            for (int i = bottom; i >= top; i--){
-                Console.Write(matrix[i, left] + " ");
-             }
-            left++;
+
+            if (left <= right) { 
+                for (int i = bottom; i >= top; i--)
+                {
+                    Console.Write(matrix[i, left] + " ");
+                }
+                left++;
             }
-            Console.WriteLine();
         }
-    }
-    static void Main(string[] args){
-    int[,] matrix = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
-    display(matrix, 3, 4);
     }
 }
